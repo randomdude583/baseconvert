db27a8e8a6a6a9812e563b83b82b543c21d76a22
randomdude583 <randomdude@wetpaintdesigns.com>
2020-05-06 00:07:50 -0500

    fixed #1

--- lib/baseconvert.dart
+++ lib/baseconvert.dart
@@ -1,82 +1,459 @@
 library baseconvert;
 
+import 'dart:math';
 
-
-base(dynamic number, int inBase, int outBase,
-    {bool string = false}){
-
-  //Ensure input is String
-  if(number is List){
-    number = number.join("");
-  } else if(!(number is String)){
-    throw Exception("Number must be List or String");
+List _stringToList(String string) {
+  //Convert string to List
+  List list = [];
+  for (int i = 0; i < string.length; i++) {
+    list.add(int.tryParse(string[i]) ?? string[i]);
   }
-
-
-  //If in and out are base10, return.
-  if(inBase == 10 && outBase ==10){
-    if(string){
-      return number;
-    } else {
-      return _stringToList(number);
-    }
-  }
-
-
-  //Convert inBase number to base10
-  int b10Number = int.parse(number, radix: inBase);
-  if(outBase == 10){
-    String output = b10Number.toString();
-    if(string){
-      return output;
-    } else {
-      return _stringToList(output);
-    }
-  } else {
-    //Convert base10 number to outBase
-    String output = b10Number.toRadixString(outBase);
-    if(string){
-      return output;
-    } else {
-      return _stringToList(output);
-    }
-  }
+  return list;
 }
 
-
-
-
-// Convert binary string to a list
-List _stringToList(String str){
-  List output = [];
-  for(int i=0; i < str.length; i++){
-    try{
-      output.add(int.parse(str[i]));
-    } catch (Exception){
-      output.add(str[i]);
-    }
-  }
+String _listToString(List list) {
+  // Convert binary string to a list
+  String output = "";
+  list.forEach((i) {
+    output += i.toString();
+  });
   return output;
 }
 
+int digit(int decimal, int digit, [int inputBase = 10]) {
+  /*
+  Find the value of an integer at a specific digit when represented in a
+  particular base.
+  Args:
+      decimal(int): A number represented in base 10 (positive integer).
+      digit(int): The digit to find where zero is the first, lowest, digit.
+      base(int): The base to use (default 10).
+  Returns:
+      The value at specified digit in the input decimal.
+      This output value is represented as a base 10 integer.*/
 
+  if (decimal == 0) {
+    return 0;
+  }
+  if (digit != 0) {
+    return (decimal ~/ pow(inputBase, digit)) % inputBase;
+  } else {
+    return decimal % inputBase;
+  }
+}
 
+int digits(int number, [int base = 10]) {
+  /*
+  Determines the number of digits of a number in a specific base.
+  Args:
+      number(int): An integer number represented in base 10.
+      base(int): The base to find the number of digits.
+  Returns:
+      Number of digits when represented in a particular base (integer).*/
+  if (number < 1) {
+    return 0;
+  }
+  int digits = 0;
+  while (number >= 1) {
+    number ~/= base;
+    digits += 1;
+  }
+  return digits;
+}
 
+List<List> integerFractionalParts(List number) {
+  /*
+  Returns a List of the integer and fractional parts of a number.
+  Args:
+      number(iterable container): A number in the following form:
+      [..., ".", int, int, int, ...]
+  Returns:
+      (integer_part, fractional_part): List.*/
 
+  int radix_point = number.indexOf(".");
+  List integer_part = number.sublist(0, radix_point);
+  List fractional_part = number.sublist(radix_point);
+  return ([integer_part, fractional_part]);
+}
 
-class BaseConvert {
+List fromBase10(int decimal, [outputBase = 10]) {
+  /*
+  Converts a decimal integer to a specific base.
+  Args:
+      decimal(int) A base 10 number.
+      outputBase(int) base to convert to.
+  Returns:
+      A list of digits in the specified base.*/
 
-  final int inBase;
-  final int outBase;
+  if (decimal <= 0) {
+    return [0];
+  }
+  if (outputBase == 1) {
+    return List.generate(outputBase, (number) => 1);
+  }
+  int length = digits(decimal, outputBase);
+  List converted = List.generate(length, (i) => digit(decimal, i, outputBase));
+  return converted.reversed.toList();
+}
+
+int toBase10(List n, int inputBase) {
+  /*
+  Converts an integer in any base into it's decimal representation.
+  Args:
+      n - An integer represented as a List of digits in the specified base.
+      input_base - the base of the input number.
+  Returns:
+      integer converted into base 10.*/
+
+  int sum = 0;
+  for (int i = 0; i < n.length; i++) {
+    sum += n.reversed.elementAt(i) * pow(inputBase, i);
+  }
+  return sum;
+}
+
+List integerBase(List number, [int inputBase = 10, int outputBase = 10]) {
+  /*
+  Converts the integer part of a number from one base to another.
+  Args:
+      number  - An number in the following form:
+          (int, int, int, ...)
+          (iterable container) containing positive integers of input base.
+      input_base    - The base to convert from.
+      output_base   - The base to convert to.
+  Returns:
+      A List of digits.*/
+
+  return fromBase10(toBase10(number, inputBase), outputBase);
+}
+
+List fractionalBase(List fractionalPart,
+    [int inputBase = 10, int outputBase = 10, int maxDepth = 100]) {
+  /*
+  Convert the fractional part of a number from any base to any base.
+  Args:
+      fractional_part(iterable container): The fractional part of a number in
+          the following form:    ( ".", int, int, int, ...)
+      input_base(int): The base to convert from (defualt 10).
+      output_base(int): The base to convert to (default 10).
+      max_depth(int): The maximum number of decimal digits to output.
+  Returns:
+      The converted number as a tuple of digits.*/
+
+  fractionalPart = fractionalPart.sublist(1);
+  int fractionalDigits = fractionalPart.length;
+  BigInt numerator = BigInt.from(0);
+  for (int i = 0; i < fractionalPart.length; i++) {
+    numerator = BigInt.from(numerator.toInt() +
+        (fractionalPart[i] * pow(inputBase, fractionalDigits - (i + 1))));
+  }
+  BigInt denominator = BigInt.from(pow(inputBase, fractionalDigits));
+  List digits = [];
+  for (int i = 1; i < maxDepth + 1; i++) {
+    numerator *= BigInt.from(pow(outputBase, i));
+    int digit = (numerator ~/ denominator).toInt();
+    numerator -= BigInt.from(digit) * denominator;
+    denominator *= BigInt.from(pow(outputBase, i));
+    digits.add(digit);
+
+    BigInt greatestCommonDivisor = numerator.gcd(denominator);
+    numerator ~/= greatestCommonDivisor;
+    denominator ~/= greatestCommonDivisor;
+  }
+  digits.insert(0, ".");
+  return digits;
+}
+
+List truncate(List n) {
+  /*
+  Removes trailing zeros.
+  Args:
+      n:  The number to truncate.
+          This number should be in the following form:
+          (..., '.', int, int, int, ..., 0)
+  Returns:
+      n with all trailing zeros removed*/
+
+  int count = 0;
+  for (int i = 0; i < n.length; i++) {
+    if (n.reversed.elementAt(i) != 0) {
+      break;
+    }
+    count += 1;
+  }
+  if (count > 0) {
+    return n.sublist(0, n.length - count);
+  } else {
+    return n;
+  }
+}
+
+int strDigitToInt(String chr) {
+  /*
+  Converts a String character to a decimal number.
+  Where "A"->10, "B"->11, "C"->12, ...etc
+  Args:
+      chr(String): A single character in the form of a String.
+  Returns:
+      The integer value of the input String digit.*/
+
+  int n;
+
+  //0-9
+  if (["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"].contains(chr)) {
+    n = int.parse(chr);
+  } else {
+    n = chr.codeUnits[0];
+  }
+
+  //A-Z
+  if (n < 91) {
+    n -= 55;
+  } else {
+    //a-z or higher
+    n -= 61;
+  }
+  return n;
+}
+
+String intToStrDigit(int n) {
+  /*
+  Converts a positive integer, to a single string character.
+  Where: 9 -> "9", 10 -> "A", 11 -> "B", 12 -> "C", ...etc
+  Args:
+      n(int): A positve integer number.
+  Returns:
+      The character representation of the input digit of value n (str).*/
+
+  //0-9
+  if (n < 10) {
+    return n.toString();
+  }
+  //A-Z
+  else if (n < 36) {
+    return String.fromCharCode(n);
+  }
+  //a-z or higher
+  else {
+    return String.fromCharCode(n + 61);
+  }
+}
+
+findRecurring(List number, {int minRepeat = 5}) {
+  /*
+  Attempts to find repeating digits in the fractional component of a number.
+  Args:
+      number(List): the number to process in the form:
+          (int, int, int, ... ".", ... , int int int)
+      min_repeat(int): the minimum number of times a pattern must occur to be
+          defined as recurring. A min_repeat of n would mean a pattern must
+          occur at least n + 1 times, so as to be repeated n times.
+  Returns:
+      The original number with repeating digits (if found) enclosed by  "["
+      and "]" (List).*/
+
+  // Return number if it has no fractional part, or minRepeat value is invalid.
+  if (!number.contains(".") || minRepeat < 1) {
+    return number;
+  }
+  //Seperate the number into integer and fractional parts.
+  List integerPart = integerFractionalParts(number)[0];
+  List fractionalPart = integerFractionalParts(number)[1];
+  //Reverse fractional part to get a sequence.
+  List sequence = fractionalPart.reversed.toList();
+  //Initialize counters
+  //The 'period' is the number of digits in a pattern.
+  int period = 0;
+  //The best pattern found will be stored.
+  int best = 0;
+  int bestPeriod = 0;
+  int bestRepeat = 0;
+  //Find recurring pattern.
+  while (period < sequence.length) {
+    period += 1;
+    List pattern = sequence.sublist(0, period - 1);
+    int repeat = 0;
+    int digit = period;
+    bool patternMatch = true;
+    while (patternMatch && digit < sequence.length) {
+      for (int i = 0; i < pattern.length; i++) {
+        if (sequence[digit + i] != pattern[i]) {
+          patternMatch = false;
+          break;
+        }
+        if (i == pattern.length - 1) {
+          repeat += 1;
+        }
+      }
+      digit += period;
+    }
+    //Give each pattern found a rank and use the best.
+    int rank = period * repeat;
+    if (rank > best) {
+      bestPeriod = period;
+      bestRepeat = repeat;
+      best = rank;
+    }
+  }
+  //If the pattern does not repeat often enough, return the original number.
+  if (bestRepeat < minRepeat) {
+    return number;
+  }
+  //Use the best pattern found.
+  List pattern = sequence.sublist(0, bestPeriod);
+  //Remove the pattern from our original number.
+  number = integerPart +
+      fractionalPart.sublist(0, fractionalPart.length - (best + bestPeriod));
+  //Ensure we are at the start of the pattern.
+  List patternTemp = pattern;
+  for (int i = 0; i < pattern.length; i++) {
+    if (number[number.length - 1] == digit) {
+      number = number.sublist(0, number.length - 2);
+      patternTemp = patternTemp.sublist(1) + (patternTemp[0]);
+    }
+    pattern = patternTemp;
+    //Return the number with the recurring pattern enclosed with '[' and ']'
+    return number + ["["] + pattern.reversed.toList() + ["]"];
+  }
+}
+
+expandRecurring(List number, {int repeat = 5}) {
+  /*
+  Expands a recurring pattern within a number.
+  Args:
+      number(List): the number to process in the form:
+          (int, int, int, ... ".", ... , int int int)
+      repeat: the number of times to expand the pattern.
+  Returns:
+      The original number with recurring pattern expanded.
+  */
+
+  if (number.contains("[")) {
+    int patternIndex = number.indexOf("[");
+    List pattern = number.sublist(patternIndex + 1, number.length - 1);
+    number = number.sublist(0, patternIndex);
+    for (int i = 0; i < repeat + 1; i++) {
+      number = number + pattern;
+    }
+  }
+  return number;
+}
+
+bool checkValid(List number, [int inputBase = 10]) {
+  /*
+  Checks if there is an invalid digit in the input number.
+  Args:
+      number: An number in the following form:
+          (int, int, int, ... , '.' , int, int, int)
+          (iterable container) containing positive integers of the input base
+      input_base(int): The base of the input number.
+  Returns:
+      bool, true if all digits valid, else false.*/
+
+  for (dynamic n in number) {
+    if ([".", "[", "]"].contains(n)) {
+      continue;
+    } else if (n >= inputBase) {
+      if (n == 1 && inputBase == 1) {
+        continue;
+      } else {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+base(dynamic number,
+    {int inputBase = 10,
+    int outputBase = 10,
+    int maxDepth = 10,
+    bool string = false,
+    bool recurring = true}) {
+  /*
+  Converts a number from any base to any another.
+  Args:
+      number(List|String|int): The number to convert.
+      inputBase(int): The base to convert from (defualt 10).
+      outputBase(int): The base to convert to (default 10).
+      maxDepth(int): The maximum number of fractional digits (defult 10).
+      string(bool): If true output will be in String representation,
+          if false output will be in List representation (defult false).
+      recurring(bool): Attempt to find repeating digits in the fractional
+          part of a number. Repeated digits will be enclosed with "[" and "]"
+          (default true).
+  Returns:
+      A List of digits in the specified base:
+      (int, int, int, ... , '.' , int, int, int)
+      If the string flag is set to true,
+      a String representation will be used instead.
+  Raises:
+      ValueError if a digit value is too high for the inputBase.*/
+
+  //Convert number to List representation
+  if (number is int || number is double) {
+    number = number.toString();
+  }
+  if (number is String) {
+    number = _stringToList(number);
+  }
+  //Check that the number is valid for the input base
+  if (!checkValid(number, inputBase)) {
+    throw Exception("Invalid!");
+  }
+  //Deal with base-1 special case
+  if (inputBase == 1) {
+    number = List.generate(number.length, (number) => 1);
+  }
+  //Expand any recurring digits
+  number = expandRecurring(number, repeat: 5);
+  //Convert a fractional number
+  if (number.contains(".")) {
+    int radixPoint = number.indexOf(".");
+    List integerPart = number.sublist(0, radixPoint);
+    List fractionalPart = number.sublist(radixPoint);
+    integerPart = integerBase(integerPart, inputBase, outputBase);
+    fractionalPart =
+        fractionalBase(fractionalPart, inputBase, outputBase, maxDepth);
+
+    number = integerPart + fractionalPart;
+    number = truncate(number);
+  }
+
+  //Convert an integer number
+  else {
+    number = integerBase(number, inputBase, outputBase);
+  }
+
+  if (recurring) {
+    number = findRecurring(number, minRepeat: 2);
+  }
+
+  //Return the converted number as a string or list
+  if (string) {
+    return _listToString(number);
+  } else {
+    return number;
+  }
+}
+
+class BaseConverter {
+  final int inputBase;
+  final int outputBase;
+  final int maxDepth;
   final bool string;
+  final bool recurring;
 
-  BaseConvert(this.inBase, this.outBase, {this.string = false});
+  BaseConverter({
+    this.inputBase = 10,
+    this.outputBase = 10,
+    this.maxDepth = 10,
+    this.string = false,
+    this.recurring = true});
 
   convert(dynamic number){
-    return base(number, inBase, outBase, string: string);
+    return base(number, inputBase: inputBase, outputBase: outputBase,
+        maxDepth: maxDepth, string: string, recurring: recurring);
   }
 
 }
-
-
-
--- test/baseconvert_test.dart
+++ test/baseconvert_test.dart
@@ -1,71 +1,239 @@
+import 'dart:math';
+
 import 'package:test/test.dart';
 import 'package:baseconvert/baseconvert.dart';
 
 void main() {
+  
+  group('digit()', (){
+    test('201_0', (){
+      expect(digit(201, 0), 1);
+    });
+    test('201_1', (){
+      expect(digit(201, 1), 0);
+    });
+    test('201_2', (){
+      expect(digit(201, 2), 2);
+    });
+    test('generate', (){
+      expect(List.generate(8, (num) {return digit(253, num, 2);}), [1, 0, 1, 1, 1, 1, 1, 1]);
+    });
+    test('hex', (){
+      expect(digit(123456789123456789, 0, 16), 5);
+    });
+  });
+
+  group('digits()', (){
+    test('255', (){
+      expect(digits(255), 3);
+    });
+    test('255_16', (){
+      expect(digits(255, 16), 2);
+    });
+    test('256_16', (){
+      expect(digits(256, 16), 3);
+    });
+    test('256_2', (){
+      expect(digits(256, 2), 9);
+    });
+    test('0_678363', (){
+      expect(digits(0, 678363), 0);
+    });
+    test('-1_678363', (){
+      expect(digits(-1, 678363), 0);
+    });
+    test('12345_10', (){
+      expect(digits(12345, 10), 5);
+    });
+  });
+
+
+  group('integerFractionalParts()',(){
+    test('[1,2,3,".",4,5,6]', (){
+      expect(integerFractionalParts([1,2,3,".",4,5,6]), [[1, 2, 3], ['.', 4, 5, 6]]);
+    });
+  });
+  
+  group('fromBase10()', (){
+    test('255', (){
+      expect(fromBase10(255), [2, 5, 5]);
+    });
+    test('255_16', (){
+      expect(fromBase10(255, 16), [15, 15]);
+    });
+    test('9988664439_8', (){
+      expect(fromBase10(9988664439, 8), [1, 1, 2, 3, 2, 7, 5, 6, 6, 1, 6, 7]);
+    });
+    test('0_17', (){
+      expect(fromBase10(0, 17), [0]);
+    });
+  });
+
+
+  group('toBase10()', (){
+    test('[8,1]_16', (){
+      expect(toBase10([8,1], 16), 129);
+    });
+  });
+
+  group('integerBase()',(){
+    test('[2, 5, 5]', (){
+      expect(integerBase([2, 5, 5]), [2, 5, 5]);
+    });
+    test('[2, 5, 4]_10_16', (){
+      expect(integerBase([2, 5, 4], 10, 16), [15, 14]);
+    });
+    test('[2, 5, 5]_10_16', (){
+      expect(integerBase([2, 5, 5], 10, 16), [15, 15]);
+    });
+    test('[3, 1, 0]_10_16', (){
+      expect(integerBase([3, 1, 0], 10, 16), [1, 3, 6]);
+    });
+    test('[11, 4, 1, 8, 10]_13_20', (){
+      expect(integerBase([11, 4, 1, 8, 10], 13, 20), [2, 0, 8, 2, 2]);
+    });
+    test('[10, 10, 1, 13]_15_20', (){
+      expect(integerBase([10, 10, 1, 13], 15, 20), [4, 10, 1, 8]);
+    });
+  });
+
+
+  group('fractionalBase()', (){
+    test('[".", 6,]_10_16_10', (){
+      expect(fractionalBase([".", 6], 10, 16, 10),
+          ['.', 9, 9, 9, 9, 9, 9, 9, 9, 9, 9]);
+    });
+  });
+
+
+  group('truncate()', (){
+    test("[9, 9, 9, '.', 9, 9, 9, 9, 0, 0, 0, 0]", (){
+      expect(truncate([9, 9, 9, '.', 9, 9, 9, 9, 0, 0, 0, 0]),
+          [9, 9, 9, '.', 9, 9, 9, 9]);
+    });
+    test("['.',]", (){
+      expect(truncate(['.',]), ['.']);
+    });
+  });
+
+
+  group("findRecurring()", (){
+    test("[3, 2, 1, '.', 1, 2, 3, 1, 2, 3], minRepeat: 1", (){
+      expect(findRecurring([3, 2, 1, '.', 1, 2, 3, 1, 2, 3], minRepeat: 1),
+          [3, 2, 1, '.', '[', 1, 2, 3, ']']);
+    });
+  });
+  
+  
+  group("expandRecurring()", (){
+    test('[1, ".", 0, "[", 9, "]"], repeat=3', (){
+      expect(expandRecurring([1, ".", 0, "[", 9, "]"], repeat: 3),
+          [1, '.', 0, 9, 9, 9, 9]);
+    });
+  });
+
+
+
+  
+  
+  group('checkValid()', (){
+    test("[1,9,6,'.',5,1,6]_12", (){
+      expect(checkValid([1,9,6,'.',5,1,6], 12), true);
+    });
+    test("[8,1,15,9]_15", (){
+      expect(checkValid([8,1,15,9], 15), false);
+    });
+  });
+
+
+  group('base()', (){
+    test("[1,9,6,'.',5,1,6], 17, 20", (){
+      expect(base([1,9,6,'.',5,1,6], inputBase: 17, outputBase: 20),
+          [1, 2, 8, '.', 5, 19, 10, 7, 17, 2, 13, 13, 1, 8]);
+    });
+  });
+
+
+  group('BaseConverter()', (){
+    group("2_4", (){
+      BaseConverter b = new BaseConverter(inputBase: 2, outputBase: 4);
+      test("", (){
+        expect(b.convert([0,1,0,1,0,1,0,0]), [1,1,1,0]);
+      });
+    });
+  });
+
+
+
+
+
+
+
+
+
+
+
+/*
   group('base', () {
     test('base_numberWrongType_exception', () {
-      expect(() => base(0, 2, 3, string: true), throwsException);
+      expect(() => baseconvert(0, 2, 3, string: true), throwsException);
     });
 
     test('base_base2IN/base3-10OUT', () {
-      expect(base("01010100", 2, 3, string: true), "10010");
-      expect(base("01010100", 2, 4, string: true), "1110");
-      expect(base("01010100", 2, 5, string: true), "314");
-      expect(base("01010100", 2, 6, string: true), "220");
-      expect(base("01010100", 2, 7, string: true), "150");
-      expect(base("01010100", 2, 8, string: true), "124");
-      expect(base("01010100", 2, 9, string: true), "103");
-      expect(base("01010100", 2, 10, string: true), "84");
+      expect(baseconvert("01010100", 2, 3, string: true), "10010");
+      expect(baseconvert("01010100", 2, 4, string: true), "1110");
+      expect(baseconvert("01010100", 2, 5, string: true), "314");
+      expect(baseconvert("01010100", 2, 6, string: true), "220");
+      expect(baseconvert("01010100", 2, 7, string: true), "150");
+      expect(baseconvert("01010100", 2, 8, string: true), "124");
+      expect(baseconvert("01010100", 2, 9, string: true), "103");
+      expect(baseconvert("01010100", 2, 10, string: true), "84");
     });
 
     test('base_base3IN/base2-10OUT', () {
-      expect(base("0012001", 3, 2, string: true), "10001000");
-      expect(base("0012001", 3, 4, string: true), "2020");
-      expect(base("0012001", 3, 5, string: true), "1021");
-      expect(base("0012001", 3, 6, string: true), "344");
-      expect(base("0012001", 3, 7, string: true), "253");
-      expect(base("0012001", 3, 8, string: true), "210");
-      expect(base("0012001", 3, 9, string: true), "161");
-      expect(base("0012001", 3, 10, string: true), "136");
+      expect(baseconvert("0012001", 3, 2, string: true), "10001000");
+      expect(baseconvert("0012001", 3, 4, string: true), "2020");
+      expect(baseconvert("0012001", 3, 5, string: true), "1021");
+      expect(baseconvert("0012001", 3, 6, string: true), "344");
+      expect(baseconvert("0012001", 3, 7, string: true), "253");
+      expect(baseconvert("0012001", 3, 8, string: true), "210");
+      expect(baseconvert("0012001", 3, 9, string: true), "161");
+      expect(baseconvert("0012001", 3, 10, string: true), "136");
     });
 
     test('base_base10IN/base10OUT', () {
-      expect(base("1945", 10, 10, string: true), "1945");
+      expect(baseconvert("1945", 10, 10, string: true), "1945");
     });
 
     test('base_base2IN/base10OUT', () {
-      expect(base("11110011001", 2, 10, string: true), "1945");
+      expect(baseconvert("11110011001", 2, 10, string: true), "1945");
     });
 
     test('base_StringIN/StringOUT', () {
-      expect(base("01010100", 2, 3, string: true), "10010");
+      expect(baseconvert("01010100", 2, 3, string: true), "10010");
     });
     test('base_List<int>IN/StringOUT', () {
-      expect(base([0, 1, 0, 1, 0, 1, 0, 0], 2, 3, string: true), "10010");
+      expect(baseconvert([0, 1, 0, 1, 0, 1, 0, 0], 2, 3, string: true), "10010");
     });
     test('base_StringIN/List<int>OUT', () {
-      expect(base("01010100", 2, 3, string: false), [1, 0, 0, 1, 0]);
+      expect(baseconvert("01010100", 2, 3, string: false), [1, 0, 0, 1, 0]);
     });
     test('base_list<int>IN/List<int>OUT', () {
-      expect(base(['F', 'F', 0], 16, 10, string: false), [4, 0, 8, 0]);
+      expect(baseconvert(['F', 'F', 0], 16, 10, string: false), [4, 0, 8, 0]);
     });
   });
 
 
   group('baseconvert', (){
     test('baseconvert_create_noExceptions', (){
-      BaseConvert b = BaseConvert(2,10);
+      BaseConverter b = BaseConverter(2,10);
     });
 
     test('baseconvert_base2IN/base4OUT', (){
-      BaseConvert b = BaseConvert(2,4, string: true);
+      BaseConverter b = BaseConverter(2,4, string: true);
       expect(b.convert("01010100"), "1110");
     });
-
-
-
-
-  });
+  });*/
 
 
 
